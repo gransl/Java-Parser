af5b8a6ddbb433c3d2e238a9b6eb6067f3de2dbb
Sandra <gran.sl@gmail.com>
2024-11-18 00:14:21 -0800

    Added parser class, but also went back and tested the one last thing I wanted to test about my Red-Black Tree add method.

--- src/test/java/TreePackage/RedBlackTreeTest.java
+++ src/test/java/TreePackage/RedBlackTreeTest.java
@@ -278,6 +278,22 @@ class RedBlackTreeTest {
         assertTrue(rightChild.isBlack());
         assertFalse(rightLeftChild.isBlack());
         assertFalse(rightRightChild.isBlack());
+
+        //additional add for coverage
+        intTree.add(29);
+        intTree.add(24);
+        intTree.add(30);
+        assertEquals(29,rightRightChild.getRightChild().getData());
+        assertEquals(24,rightRightChild.getLeftChild().getData());
+        assertEquals(30,rightRightChild.getRightChild().getRightChild().getData());
+        assertFalse(rootNode.isBlack()); //13, no longer the root
+        assertTrue(leftChild.isBlack()); //7
+        assertTrue(rightChild.isBlack()); //23, new root
+        assertTrue(rightLeftChild.isBlack()); //20
+        assertFalse(rightRightChild.isBlack()); //26
+        assertTrue(rightRightChild.getRightChild().isBlack()); //29
+        assertTrue(rightRightChild.getLeftChild().isBlack()); //24
+        assertFalse(rightRightChild.getRightChild().getRightChild().isBlack()); //30
     }
 
     @Test
@@ -313,6 +329,53 @@ class RedBlackTreeTest {
         assertTrue(rightChild.isBlack());
         assertFalse(leftLeftChild.isBlack());
         assertFalse(leftRightChild.isBlack());
+    }
+
+    @Test
+    void extraRotationTest() {
+        RedBlackTree<Integer> intTree = new RedBlackTree(13);
+        assertEquals(13, intTree.getRootData());
+        intTree.add(8);
+        intTree.add(26);
+        intTree.add(10);
+        intTree.add(5);
+        intTree.add(2);
+        intTree.add(6);
+        intTree.add(-3);
+        RedBlackNode<Integer> rootNode = intTree.getRootNode();
+        RedBlackNode<Integer> leftChild = rootNode.getLeftChild();
+        RedBlackNode<Integer> rightChild = rootNode.getRightChild();
+        RedBlackNode<Integer> leftLeftChild = leftChild.getLeftChild();
+        RedBlackNode<Integer> leftRightChild = leftChild.getRightChild();
+        RedBlackNode<Integer> rightLeftChild = rightChild.getLeftChild();
+        RedBlackNode<Integer> rightRightChild = rightChild.getRightChild();
+
+        //check values & relationships
+        assertEquals(8, intTree.getRootData());
+        assertNull(rootNode.getParent());
+        assertEquals(5, leftChild.getData());
+        assertEquals(13, rightChild.getData());
+        assertEquals(8, leftChild.getParent().getData());
+        assertEquals(8, rightChild.getParent().getData());
+        assertEquals(2, leftLeftChild.getData());
+        assertEquals(6, leftRightChild.getData());
+        assertEquals(5, leftLeftChild.getParent().getData());
+        assertEquals(5, leftRightChild.getParent().getData());
+        assertEquals(10, rightLeftChild.getData());
+        assertEquals(26, rightRightChild.getData());
+        assertEquals(13, rightLeftChild.getParent().getData());
+        assertEquals(13, rightRightChild.getParent().getData());
+        assertEquals(-3, leftLeftChild.getLeftChild().getData());
+
+        //check colors
+        assertTrue(rootNode.isBlack()); //8
+        assertFalse(leftChild.isBlack()); //5
+        assertFalse(rightChild.isBlack()); //13
+        assertTrue(leftLeftChild.isBlack()); //2
+        assertTrue(leftRightChild.isBlack()); //4
+        assertTrue(rightLeftChild.isBlack()); //10
+        assertTrue(rightRightChild.isBlack()); //26
+        assertFalse(leftLeftChild.getLeftChild().isBlack()); //-3
     }
 
     @Test
