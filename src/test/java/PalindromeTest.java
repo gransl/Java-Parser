b2ca64f086fa389182da84f77f95238d65682a91
Sandra <gran.sl@gmail.com>
2024-11-18 16:06:37 -0800

    Finished testing Palindrome, deleted clases that were not being used for my project

---
+++ src/test/java/PalindromeTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+
+    @Test
+    void main() {
+        // Save the original System.out
+        PrintStream originalOut = System.out;
+
+        // Create a ByteArrayOutputStream to capture the output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream testOut = new PrintStream(outputStream);
+
+        // Redirect System.out to the test PrintStream
+        System.setOut(testOut);
+
+        // Code to test
+        String[] testArgs = {};
+        Palindrome.main(testArgs);
+
+        // Restore the original System.out
+        System.setOut(originalOut);
+
+        String testOutString = outputStream.toString();
+        Scanner output = new Scanner(testOutString);
+
+        assertTrue(Boolean.parseBoolean(output.next()));
+        assertFalse(output.hasNext());
+    }
+
+    @Test
+    void isPalindrome() {
+        assertTrue(Palindrome.isPalindrome("civic"));
+        assertTrue(Palindrome.isPalindrome("a"));
+        assertTrue(Palindrome.isPalindrome("RotAtoR"));
+        assertTrue(Palindrome.isPalindrome("nAAn"));
+        assertTrue(Palindrome.isPalindrome("aibohphobia"));
+        assertTrue(Palindrome.isPalindrome("abcdefghhgfedcba"));
+        assertTrue(Palindrome.isPalindrome("abcdefghihgfedcba"));
+        assertTrue(Palindrome.isPalindrome(""));
+        assertFalse(Palindrome.isPalindrome("hello"));
+        assertFalse(Palindrome.isPalindrome("Civic"));
+        assertFalse(Palindrome.isPalindrome("nAan"));
+        assertFalse(Palindrome.isPalindrome("friday"));
+        assertFalse(Palindrome.isPalindrome("oroboros"));
+        assertFalse(Palindrome.isPalindrome("marjoram"));
+        assertFalse(Palindrome.isPalindrome("laconical"));
+        assertFalse(Palindrome.isPalindrome("xerox"));
+        assertFalse(Palindrome.isPalindrome("foolproof"));
+    }
+}
